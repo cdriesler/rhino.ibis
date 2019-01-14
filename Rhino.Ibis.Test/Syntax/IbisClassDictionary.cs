6645734314839f9fdc7eaa5636020e1140cd5e3e
Chuck Driesler <cdriesler.iv@gmail.com>
2019-01-14 18:00:12 -0500

    add curve functionality

--- Rhino.Ibis.Test/Syntax/IbisClassDictionary.cs
+++ Rhino.Ibis.Test/Syntax/IbisClassDictionary.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Rhino.Ibis.Relations;
+using Rhino.Ibis.Reviews.Curve;
 using Rhino.Ibis.Reviews.Curves;
 using Rhino.Ibis.Reviews.CurveToCurve;
 
@@ -13,12 +14,14 @@ namespace Rhino.Ibis.Test.Syntax
     {
         public static List<Type> AllReviews { get; set; } = new List<Type>()
         {
+            typeof(CurveReview),
             typeof(CurvesReview),
             typeof(CurveToCurveReview)
         };
 
         public static List<Type> AllReviewOptions { get; set; } = new List<Type>()
         {
+            typeof(CurveReviewOptions),
             typeof(CurvesReviewOptions),
             typeof(CurveToCurveReviewOptions)
         };
