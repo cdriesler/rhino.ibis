28708358687bf94ffdc75b62e8a73b25b5175ab0
Chuck Driesler <cdriesler.iv@gmail.com>
2019-01-17 14:09:34 -0500

    template output to separate files

---
+++ Rhino.Ibis/Templates/CurvesRelation.cs
@@ -0,0 +1,18 @@
+
+using System;
+using System.Collections.Generic;
+using Rhino.Geometry;
+using Rhino.Ibis.Logic.Curves;
+
+namespace Rhino.Ibis.TextTemplateTest
+{
+
+	public class CurvesRelation
+	{
+		//Related geometry
+		private List<Curve> TheseGeometry { get; set; }
+						
+	}
+
+}
+
---
+++ Rhino.Ibis/Templates/RegionRelation.cs
@@ -0,0 +1,18 @@
+
+using System;
+using System.Collections.Generic;
+using Rhino.Geometry;
+using Rhino.Ibis.Logic.Region;
+
+namespace Rhino.Ibis.TextTemplateTest
+{
+
+	public class RegionRelation
+	{
+		//Related geometry
+		private Region ThisGeometry { get; set; }
+						
+	}
+
+}
+
