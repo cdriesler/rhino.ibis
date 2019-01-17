0508920e106228d8c0ed28932554ca3a83289ead
Chuck Driesler <cdriesler.iv@gmail.com>
2019-01-17 14:00:34 -0500

    basic scaffold

---
+++ Rhino.Ibis/Logic/Library.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Rhino.Ibis.Logic.Curves;
+using Rhino.Ibis.Logic.Region;
+
+namespace Rhino.Ibis.Logic
+{
+    public static class Library
+    {
+        public static List<Type> GetAvailableRelations()
+        {
+            return new List<Type>
+            {
+                typeof(CurvesLogic),
+                typeof(RegionLogic)
+            };
+        }
+    }
+}
